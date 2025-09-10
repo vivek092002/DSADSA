3890e5d7aa75cd254ade2fb01ac85ba30b7c72c6
VIVEK KUMAR <103557229+vivek092002@users.noreply.github.com>
2025-09-11 00:39:42 +0530

    Add files via upload

--- arrays/removeDuplicates.java
+++ arrays/removeDuplicates.java
@@ -7,7 +7,7 @@ public class removeDuplicates {
     public static void main(String[] args) {
         int[] arr = {1,1,2,2,2,3,3};
         int n = arr.length;
-        System.out.println(remove(arr,n));
+        System.out.println(remove2(arr,n));
     }
 
     //brute force approach
@@ -27,5 +27,23 @@ public class removeDuplicates {
             arr[j++] = x;
         }
         return k;
+    }
+
+    //optimised solution
+    //Intuition: We can think of using two pointers ‘i’ and ‘j’, we move ‘j’ till we don't get a number arr[j] which is different from arr[i]. As we got a unique number we will increase the i pointer and update its value by arr[j].
+    //Approach:
+    //Take a variable i as 0;
+    //Use a for loop by using a variable ‘j’ from 1 to length of the array.
+    //If arr[j] != arr[i], increase ‘i’ and update arr[i] == arr[j].
+    // After completion of the loop return i+1, i.e size of the array of unique elements.
+    public static int remove2(int[] arr, int n){
+        int i = 0;
+        for (int j = 1 ; j < arr.length; j++){
+            if (arr[j] != arr[i]){
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
     }
 }
